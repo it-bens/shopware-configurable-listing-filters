19ac5ad593dd4b45a1c9c8e4dbe37f3dd837f5a6
Martin Bens <martin.bens@it-bens.de>
2025-04-09 17:07:29 +0200

    Console commands removed because they were primarily used for initial development testing.

--- src/DependencyInjection/services.php
+++ src/DependencyInjection/services.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import('services/console.php');
     $containerConfigurator->import('services/core.php');
     $containerConfigurator->import('services/listing_filter.php');
     $containerConfigurator->import('services/twig.php');
